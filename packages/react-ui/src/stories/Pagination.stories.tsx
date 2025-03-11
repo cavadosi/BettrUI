05ca606fb120d17600d52032b81a885579de5748
cavadosi <jpeque.moab@gmail.com>
2025-03-11 19:23:32 +0100

    Working pagination

---
+++ packages/react-ui/src/stories/Pagination.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Pagination, PaginationProps } from "../components/Pagination";
+
+export default {
+  title: "React/Pagination",
+  component: Pagination,
+  argTypes: {
+    totalPages: { control: { type: "number" } },
+    max: { control: { type: "number" } },
+  },
+};
+
+const DefaultTemplate = (args: React.ComponentProps<typeof Pagination>) => {
+  const [currentPage, setCurrentPage] = React.useState(args.currentPage);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <Pagination
+      {...args}
+      currentPage={currentPage}
+      onPageChange={handlePageChange}
+    />
+  );
+};
+
+export const Default = DefaultTemplate.bind({});
+Default.args = {
+  totalPages: 10,
+  currentPage: 1,
+  max: 11,
+};
