354673754a0b8a104a9cf108ba02989e1e9818ff
macladi <dcastillonsoler@gmail.com>
2025-03-27 09:51:32 +0100

    Feat:avatar vue-97

---
+++ packages/vue/src/stories/Avatar.stories.ts
@@ -0,0 +1,38 @@
+import Avatar from "../components/Avatar.vue";
+
+export default {
+  title: "Vue/Avatar",
+  component: Avatar,
+  argTypes: {
+    size: {
+      control: { type: "select" },
+      options: ["xs", "sm", "md", "lg", "xl"],
+    },
+    rounded: {
+      control: { type: "select" },
+      options: ["none", "sm", "md", "lg", "xl", "full"],
+    },
+    src: {
+      control: { type: "text" },
+    },
+    alt: {
+      control: { type: "text" },
+    },
+  },
+};
+
+const Template = (args) => ({
+  components: { Avatar },
+  setup() {
+    return { args };
+  },
+  template: '<Avatar v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  src: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  alt: "Avatar",
+  size: "md",
+  rounded: "md",
+};
