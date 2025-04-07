7a89fd5e9d48578e09992aee0505838b6584d834
macladi <dcastillonsoler@gmail.com>
2025-04-07 22:34:49 +0200

    Container component

---
+++ packages/vue/lib/Container/Container.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { twMerge } from "tailwind-merge";
+
+const props = defineProps<{
+  class?: string;
+  children?: string;
+}>();
+</script>
+
+<template>
+  <div :class="twMerge('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', props.class)">
+    <slot>
+        {{ props.children }}
+    </slot>
+  </div>
+</template>
+    
