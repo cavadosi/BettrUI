55dec0519e92c860ceb83e1e910a907434ee53f9
cavadosi <jpeque.moab@gmail.com>
2025-04-16 12:34:06 +0200

    Feat (docs): Migrate docs to v4 and add ui library (CAV-141)

--- docs/plugins/bettr-ui.ts
+++ docs/plugins/bettr-ui.ts
@@ -1,8 +1,7 @@
-import '@bettr-ui/vue/styles';
-import { Button } from '@bettr-ui/vue';
+import { Badge, Button } from '@bettr-ui/vue';
 
 export default defineNuxtPlugin((nuxtApp) => {
   // Register the component globally.
-  // Make sure the name matches what you use in your templates.
   nuxtApp.vueApp.component('BtrButton', Button);
+  nuxtApp.vueApp.component('BtrBadge', Badge);
 });
