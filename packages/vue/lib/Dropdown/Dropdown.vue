ae85e183893ca2ac8d2e90441a061c4e4f02f88d
macladi <dcastillonsoler@gmail.com>
2025-04-22 20:47:10 +0200

    copied dropdown from main

--- packages/vue/lib/Dropdown/Dropdown.vue
+++ packages/vue/lib/Dropdown/Dropdown.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
-import { usePopper } from 'vue-popper';
+import { usePopper } from 'vue-popperjs';
 import { twMerge } from 'tailwind-merge';
 import Icon from '../Icon/Icon.vue';
 import Button from '../Button/Button.vue';
