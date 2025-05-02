5c3bc44a2d170ffb1fd71c227cc42deaca0471cc
cavadosi <jpeque.moab@gmail.com>
2025-05-02 15:11:40 +0200

    Feat (docs): Create Dropdown docs page (CAV-153)

---
+++ docs/components/demo/DropdownDemo.vue
@@ -0,0 +1,66 @@
+<template>
+  <BtrDropdown
+    v-if="multisection"
+    icon="Cog"
+    label="Options"
+    header-title="Settings"
+    header-description="Manage your preferences"
+    button-variant="secondary"
+    button-size="sm"
+    button-rounded="md"
+    :sections="multiSections"
+  />
+  <BtrDropdown
+    v-else
+    icon="Cog"
+    label="Options"
+    button-variant="secondary"
+    button-size="sm"
+    button-rounded="md"
+    :sections="sections"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps({
+  multisection: {
+    type: Boolean,
+    default: false,
+  },
+});
+const sections = [
+  {
+    items: [
+      { id: 1, label: 'Profile' },
+      { id: 2, label: 'Settings' },
+      { id: 3, label: 'Logout' },
+    ],
+  },
+];
+
+const multiSections = [
+  {
+    items: [
+      {
+        id: 1,
+        label: 'Account',
+        icon: 'User',
+      },
+      {
+        id: 2,
+        label: 'Preferences',
+        icon: 'Cog',
+      },
+    ],
+  },
+  {
+    items: [
+      {
+        id: 3,
+        label: 'Logout',
+        icon: 'Logout',
+      },
+    ],
+  },
+];
+</script>
